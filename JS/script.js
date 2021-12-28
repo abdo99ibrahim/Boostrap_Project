85632e5d56944354dc9c210cd0ddbb2559e6768f
abdo99ibrahim <abdelramin30@gmail.com>
2021-12-28 21:11:58 +0200

    Project Bootsrap

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+(function () {
+  'use strict'
+
+  var forms = document.querySelectorAll('.needs-validation')
+
+  Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+          form.addEventListener('submit', function (event) {
+              if (!form.checkValidity()) {
+                  event.preventDefault()
+                  event.stopPropagation()
+              }
+
+              form.classList.add('was-validated')
+          }, false)
+      })
+})()
+new Swiper('.slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
